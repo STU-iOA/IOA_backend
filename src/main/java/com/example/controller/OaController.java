2a3a32b0d15065ae4734df1e2b62a042b0e42d6b
2582266840 <19jhluo@stu.edu.cn>
2021-11-05 11:01:57 +0800

    11.5

--- src/main/java/com/example/controller/OaController.java
+++ src/main/java/com/example/controller/OaController.java
@@ -1,9 +1,19 @@
 package com.example.controller;
 
 
+import com.example.demo.OADto;
+import com.example.generator.entity.Oa;
+import com.example.generator.services.DepartmentService;
+import com.example.generator.services.OAService;
+import com.example.result.Result;
+import com.example.result.ResultFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * <p>
@@ -16,6 +26,18 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/generator/oa")
 public class OaController {
-
+    @Autowired
+    public OAService oaService;
+    @Autowired
+    public DepartmentService departmentService;
+    @RequestMapping(value = "/getOA", method = RequestMethod.GET)
+    public Result getOA(@RequestParam Long OAId){
+        Oa oa=oaService.getOA(OAId);
+        OADto oaDto=new OADto();
+        BeanUtils.copyProperties(oa,oaDto);
+        oaDto.setDepartment(departmentService.getDepartment(oa.getDepartmentId()).getName());
+        return ResultFactory.buildSuccessResult(oaDto);
+    }
+    
 }
 
